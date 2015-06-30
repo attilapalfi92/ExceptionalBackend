8348d37eb3af6018c818ec5f88a1e39996bc11d4
Attila Pálfi <attila.palfi.92@gmail.com>
2015-06-30 23:22:43 +0200

    not working but must back up

--- src/main/java/com/attilapalf/exceptional/controllers/ExceptionController.java
+++ src/main/java/com/attilapalf/exceptional/controllers/ExceptionController.java
@@ -3,6 +3,7 @@ package com.attilapalf.exceptional.controllers;
 import com.attilapalf.exceptional.businessLogic.ExceptionBusinessLogic;
 import com.attilapalf.exceptional.wrappers.ExceptionSentResponse;
 import com.attilapalf.exceptional.wrappers.ExceptionWrapper;
+import com.attilapalf.exceptional.wrappers.SpringData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Attila on 2015-06-13.
@@ -24,6 +26,8 @@ public class ExceptionController {
 
     @RequestMapping(value = "/exception", method = RequestMethod.POST)
     public ResponseEntity<ExceptionSentResponse> sendException(@RequestBody ExceptionWrapper exceptionWrapper) {
+        RestTemplate restTemplate = new RestTemplate();
+        SpringData data = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", SpringData.class);
 
         return new ResponseEntity<>(exceptionBusinessLogic.sendException(exceptionWrapper), HttpStatus.OK);
     }
